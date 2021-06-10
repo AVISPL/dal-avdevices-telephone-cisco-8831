4cfab4691ce4a28e1f4a0eaac98659fd0946bac5
JaydenL-Insight <61191252+JaydenL-Insight@users.noreply.github.com>
2021-06-10 15:33:46 +1000

    Update JavaDocs and precompile regex

--- src/main/java/com/insightsystems/dal/cisco/CP8831.java
+++ src/main/java/com/insightsystems/dal/cisco/CP8831.java
@@ -3,14 +3,51 @@ package com.insightsystems.dal.cisco;
 import com.avispl.symphony.api.dal.dto.monitor.ExtendedStatistics;
 import com.avispl.symphony.api.dal.dto.monitor.Statistics;
 import com.avispl.symphony.api.dal.monitor.Monitorable;
-import com.avispl.symphony.api.dal.ping.Pingable;
 import com.avispl.symphony.dal.communicator.HttpCommunicator;
 
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CP8831 extends HttpCommunicator implements Monitorable, Pingable {
+/**
+ * Cisco CP8831 Phone Device Adapter
+ * Company: Insight Systems
+ * @author Jayden Loone (@JaydenLInsight)
+ * @version 0.2
+ *
+ * No control possible with this device.
+ *
+ * Monitored Statistic:
+ *  - MAC Address
+ *  - Hostname
+ *  - Phone Directory Number
+ *  - Firmware Version
+ *  - Hardware Revision
+ *  - Serial Number
+ *  - Model Number
+ *  - Messages Waiting
+ *  - Device Timezone
+ *  - System Free Memory
+ *  - Java Heap Free Memory
+ *  - Java Pool Free Memory
+ *  - Time and Date
+ */
+public class CP8831 extends HttpCommunicator implements Monitorable {
+    public static Pattern MacAddressPattern = Pattern.compile("MAC Address.+?<B>([^<]+)</B>");
+    public static Pattern HostnamePattern = Pattern.compile("Host Name.+?<B>([^<]+)</B>");
+    public static Pattern PhoneDnPattern = Pattern.compile("Phone DN.+?<B>([^<]+)</B>");
+    public static Pattern VersionPattern = Pattern.compile("Version.+?<B>([^<]+)</B>");
+    public static Pattern HardwareRevisionPattern = Pattern.compile("Hardware Revision.+?<B>([^<]+)</B>");
+    public static Pattern SerialNumberPattern = Pattern.compile("Serial Number.+?<B>([^<]+)</B>");
+    public static Pattern ModelNumberPattern = Pattern.compile("Model Number.+?<B>([^<]+)</B>");
+    public static Pattern MessageWaitingPattern = Pattern.compile("Message Waiting.+?<B>([^<]+)</B>");
+    public static Pattern TimeZonePattern = Pattern.compile("Time Zone.+?<B>([^<]+)</B>");
+    public static Pattern SystemFreeMemoryPattern = Pattern.compile("System Free Memory.+?<B>([^<]+)</B>");
+    public static Pattern JavaHeapFreeMemoryPattern = Pattern.compile("Java Heap Free Memory.+?<B>([^<]+)</B>");
+    public static Pattern JavaPoolFreeMemoryPattern = Pattern.compile("Java Pool Free Memory.+?<B>([^<]+)</B>");
+    public static Pattern timePattern = Pattern.compile("Time.+?<B>([^<]+)</B>");
+    public static Pattern datePattern = Pattern.compile("Date.+?<B>([^<]+)</B>");
+    
     protected void authenticate() {}
 
     @Override
@@ -20,27 +57,33 @@ public class CP8831 extends HttpCommunicator implements Monitorable, Pingable {
 
         final String res = doGet("/");
 
-        stats.put("MacAddress",regexFind(res,"MAC Address.+?<B>([^<]+)<\\/B>"));
-        stats.put("Hostname",regexFind(res,"Host Name.+?<B>([^<]+)<\\/B>"));
-        stats.put("PhoneDn",regexFind(res,"Phone DN.+?<B>([^<]+)<\\/B>"));
-        stats.put("Version",regexFind(res,"Version.+?<B>([^<]+)<\\/B>"));
-        stats.put("HardwareRevision",regexFind(res,"Hardware Revision.+?<B>([^<]+)<\\/B>"));
-        stats.put("SerialNumber",regexFind(res,"Serial Number.+?<B>([^<]+)<\\/B>"));
-        stats.put("ModelNumber",regexFind(res,"Model Number.+?<B>([^<]+)<\\/B>"));
-        stats.put("MessageWaiting",regexFind(res,"Message Waiting.+?<B>([^<]+)<\\/B>"));
-        stats.put("TimeZone",regexFind(res,"Time Zone.+?<B>([^<]+)<\\/B>"));
-        stats.put("SystemFreeMemory",regexFind(res,"System Free Memory.+?<B>([^<]+)<\\/B>"));
-        stats.put("JavaHeapFreeMemory",regexFind(res,"Java Heap Free Memory.+?<B>([^<]+)<\\/B>"));
-        stats.put("JavaPoolFreeMemory",regexFind(res,"Java Pool Free Memory.+?<B>([^<]+)<\\/B>"));
+        stats.put("MacAddress",regexFind(res,MacAddressPattern));
+        stats.put("Hostname",regexFind(res,HostnamePattern));
+        stats.put("PhoneDn",regexFind(res,PhoneDnPattern));
+        stats.put("Version",regexFind(res,VersionPattern));
+        stats.put("HardwareRevision",regexFind(res,HardwareRevisionPattern));
+        stats.put("SerialNumber",regexFind(res,SerialNumberPattern));
+        stats.put("ModelNumber",regexFind(res,ModelNumberPattern));
+        stats.put("MessageWaiting",regexFind(res,MessageWaitingPattern));
+        stats.put("TimeZone",regexFind(res,TimeZonePattern));
+        stats.put("SystemFreeMemory",regexFind(res,SystemFreeMemoryPattern));
+        stats.put("JavaHeapFreeMemory",regexFind(res,JavaHeapFreeMemoryPattern));
+        stats.put("JavaPoolFreeMemory",regexFind(res,JavaPoolFreeMemoryPattern));
 
-
-        stats.put("TimeDate",regexFind(res,"Time.+?<B>([^<]+)<\\/B>") + " " + regexFind(res,"Date.+?<B>([^<]+)<\\/B>"));
+        stats.put("TimeDate",regexFind(res,timePattern) + " " + regexFind(res,datePattern));
 
         extStats.setStatistics(stats);
         return Collections.singletonList(extStats);
     }
-    private String regexFind(String sourceString,String regex) {
-        final Matcher matcher = Pattern.compile(regex).matcher(sourceString);
+
+    /**
+     * Finds a regular expression within a String and returns the first group
+     * @param sourceString String to be searched
+     * @param regex Regular expression to search for in string
+     * @return First group matching the regular expression or an empty string if not found
+     */
+    private String regexFind(String sourceString,Pattern regex) {
+        Matcher matcher = regex.matcher(sourceString);
         return matcher.find() ? matcher.group(1) : "";
     }
 }
